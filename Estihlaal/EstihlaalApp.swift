e95cd6d253d951876028ca2b996a8fa3d5275ae5
rubamei1 <rubameshal12@hotmail.com>
2025-03-03 13:44:04 +0300

    Initial Commit

---
+++ Estihlaal/EstihlaalApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstihlaalApp.swift
+//  Estihlaal
+//
+//  Created by Ruba Meshal Alqahtani on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct EstihlaalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
