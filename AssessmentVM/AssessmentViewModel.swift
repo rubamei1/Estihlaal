de4335366af1f131bf329d6995998ec144cb3ffc
maryambsq <maryamx399@gmail.com>
2025-03-10 12:38:27 +0300

    first draft

---
+++ AssessmentVM/AssessmentViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AssessmentViewModel.swift
+//  Estihlaal
+//
+//  Created by Maryam Amer Bin Siddique on 09/09/1446 AH.
+//
+
+import SwiftUI
+
+struct AssessmentViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssessmentViewModel()
+}
---
+++ Estihlaal/AssessmentView/AssessmentQ4.swift
@@ -0,0 +1,18 @@
+//
+//  AssessmentQ4.swift
+//  Estihlaal
+//
+//  Created by Maryam Amer Bin Siddique on 06/09/1446 AH.
+//
+
+import SwiftUI
+
+struct AssessmentQ4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssessmentQ4()
+}
